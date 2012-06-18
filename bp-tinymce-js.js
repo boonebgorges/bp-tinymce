89a2e5158fd7e973d52966e0f64dc8cd3e5b7393
Boone Gorges <boonebgorges@gmail.com>
2012-06-18 15:39:19 -0400

    Round one of Kettering rewrite

--- bp-tinymce-js.js
+++ bp-tinymce-js.js
@@ -1,19 +1,39 @@
-jQuery(document).ready( function() {
-	var j = jQuery;
 
-	j("#whats-new-options").animate({height:'40px'});
-	j("form#whats-new-form textarea").animate({height:'50px'});
-	j("#aw-whats-new-submit").prop("disabled", false);
 
-	/* Activity update AJAX posting */
-	j("input#aw-whats-new-submit").click( function() {
-		tinyMCE.triggerSave();
-		j('#whats-new_ifr').contents().find('#tinymce').html('');
+
+jQuery(document).ready( function($) {
+
+	var eid = '';
+	var thisarea = '';
+	$('textarea').each(function(i,v){
+		eid = $(v).attr('id');
+		$.ajax({
+			url: ajaxurl,
+			data: { action: 'bp_tinymce_editor', eid: eid, area_content: $(v).html() },
+			success: function(data){
+				$(v).replaceWith(data);
+
+				// For some reason we need to get a fresh id
+				var ps = $(data).attr('id').split('-');
+				var the_true_id = ps[1];
+
+				//var foo = tinyMCE.execCommand("mceAddControl", true, v);
+
+				// Switch to Visual
+				switchEditors.go( the_true_id, 'tmce' );
+
+				// Manually remove inline height. Groan.
+				$('iframe#' + the_true_id + '_ifr').css('height', '');
+
+				// Hack. If this is a message reply, keep it from submitting via
+				// AJAX
+				if ( the_true_id == 'message_content' ) {
+					var send_button = $('#send_reply_button');
+					if ( 0 != send_button.length ) {
+						$(send_button).unbind('click');
+					}
+				}
+			}
+		});
 	});
-
-	/* Message reply AJAX posting */
-	j("input#send_reply_button").click( function() {
-		tinyMCE.triggerSave();
-		j('#whats-new_ifr').contents().find('#tinymce').html('');
-	});
-});
+},jQuery);
--- bp-tinymce.php
+++ bp-tinymce.php
@@ -9,52 +9,77 @@ class BP_TinyMCE {
 	var $is_teeny;
 	var $init_filter;
 
+	var $textarea_id;
+
+	var $enabled_components;
+
 	function bp_tinymce() {
 		$this->__construct();
 	}
 
 	function __construct() {
-		add_action( 'bp_before_container', array( $this, 'add_js' ), 1 );
-		add_action( 'init', array( $this, 'enqueue_script' ), 1 );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_script' ), 1 );
+
+		add_action( 'wp_ajax_bp_tinymce_editor', array( &$this, 'handler' ) );
 
 		$this->is_teeny = apply_filters( 'bp_tinymce_is_teeny', false );
 		$this->init_filter = $this->is_teeny ? 'teeny_mce_before_init' : 'tiny_mce_before_init';
 
-		// Some components have filterable allowedtags lists.
-		$tinymce_components = array(
+		$this->enabled_components = apply_filters( 'bp_tinymce_enabled_components', array(
 			'forums',
 			'activity',
-			'groups'
-		);
+			'xprofile',
+			'groups',
+			'messages'
+		) );
 
-		foreach( $tinymce_components as $component  ) {
+		foreach( $this->enabled_components as $component  ) {
 			add_filter( 'bp_forums_allowed_tags', array( $this, 'allowed_tags' ), 1 );
 			add_filter( 'bp_activity_allowed_tags', array( $this, 'allowed_tags' ), 1 );
 			add_filter( 'bp_' . $component . "_allowed_tags", array( $this, 'allowed_tags' ), 1 );
 			add_filter( "bp_" . $component . "_filter_kses", array( $this, 'allowed_tags' ), 1 );
 		}
+
+		// Hack
+		if ( in_array( 'messages', $this->enabled_components ) ) {
+			add_action( 'bp_actions', array( &$this, 'messages_send_hack' ), 1 );
+		}
 	}
 
-	function add_js() {
-		global $bp;
+	function handler() {
+		$args = array(
+			'media_buttons' => false,
+			'teensy' => isset( $_REQUEST['teensy'] ) && '1' == $_REQUEST['teensy'] ? true : false
+		);
 
-		$baseurl = includes_url( 'js/tinymce' );
+		// This is not ideal, but we need to keep double escaping from happening
+		add_filter( 'the_editor_content', function( $content ) {
+			remove_filter('the_editor_content', 'wp_richedit_pre');
+			return $content;
+		}, 8 );
 
-		if ( $this->enable_tinymce_on_page() ) {
-			wp_tiny_mce( $this->is_teeny, array( 'mode' => 'textareas' ) );
-		}
-
+		echo wp_editor( $_REQUEST['area_content'], $_REQUEST['eid'], $args );
+		die();
 	}
 
 	function enqueue_script() {
 		global $bp;
 
 		if ( $this->enable_tinymce_on_page() ) {
-			add_filter( $this->init_filter, array( $this, 'tinymce_init_params' ) );
 
-			require_once( ABSPATH . '/wp-admin/includes/post.php' );
+			if ( ! class_exists('_WP_Editors' ) )
+				require_once( ABSPATH . WPINC . '/class-wp-editor.php' );
 
-			wp_enqueue_script( 'editor' );
+			$set = array(
+				'teeny' => $this->is_teeny,
+				'tinymce' => array( 'mode' => 'exact' ),
+				'quicktags' => false
+			);
+
+			foreach( (array) $this->textarea_id as $tid ) {
+				$set = _WP_Editors::parse_settings( $tid, $set );
+				_WP_Editors::editor_settings( $tid, $set );
+			}
 
 			// We have to deregister the DTheme ajax and reregister it to be dependent
 			// on our own, so that our click events are registered before bp-default's
@@ -64,6 +89,8 @@ class BP_TinyMCE {
 			wp_register_script('bp-tinymce-js', WP_PLUGIN_URL . '/bp-tinymce/bp-tinymce-js.js', array( 'jquery' ) );
 			wp_enqueue_script( 'bp-tinymce-js' );
 
+			//wp_localize_script( 'bp-tinymce-js', 'BP_TinyMCE', $textarea_whitelist );
+
 			// Reload bp-default ajax
 			wp_enqueue_script( 'dtheme-ajax-js', get_template_directory_uri() . '/_inc/global.js', array( 'jquery', 'bp-tinymce-js' ) );
 
@@ -72,60 +99,98 @@ class BP_TinyMCE {
 		}
 	}
 
-	function tinymce_init_params( $initArray ) {
-		$plugins = explode( ',', $initArray['plugins'] );
+	function enable_tinymce_on_page() {
+		$enable = false;
 
-		// Remove internal linking
-		$wplink_key = array_search( 'wplink', $plugins );
-		if ( $wplink_key ) {
-			unset( $plugins[$wplink_key] );
+		// Group forums
+		if ( bp_is_group_forum() && in_array( 'forums', $this->enabled_components ) ) {
+			if ( bp_is_group_forum_topic() ) {
+				$this->textarea_id = 'reply_text';
+			} else {
+				$this->textarea_id = 'topic_text';
+			}
 		}
 
-		// Important. Allows arbitrary textareas to be selected
-		unset( $initArray['editor_selector'] );
+		// Group edit-details (description)
+		if ( bp_is_group_admin_page() && bp_is_action_variable( 'edit-details', 0 ) && in_array( 'groups', $this->enabled_components ) ) {
+			$this->textarea_id = 'group-desc';
+		}
 
-		$plugins = array_values( $plugins );
+		// Messages
+		if ( bp_is_messages_component() && in_array( 'messages', $this->enabled_components ) ) {
+			$this->textarea_id = 'message_content';
+		}
 
-		$initArray['plugins'] = implode( ',', $plugins );
+		// Profile edit
+		if ( bp_is_user_profile_edit() && in_array( 'xprofile', $this->enabled_components ) ) {
+			$args = array(
+				'hide_empty_fields' => false,
+				'fetch_field_data' => false
+			);
 
-		return apply_filters( 'bp_tinymce_init_parms', $initArray );
-	}
+			if ( bp_has_profile( $args ) ) {
+				while ( bp_profile_groups() ) {
+					bp_the_profile_group();
 
+					global $profile_template;
+					foreach( (array) $profile_template->group->fields as $f ) {
+						if ( 'textarea' == $f->type ) {
+							$this->textarea_id[] = 'field_' . $f->id;
+						}
+					}
+				}
+			}
+		}
 
-	function enable_tinymce_on_page() {
 		// todo
-		return apply_filters( 'bp_tinymce_enable_on_page', true );
+		return apply_filters( 'bp_tinymce_enable_on_page', (bool) $this->textarea_id );
 	}
 
 	function allowed_tags( $allowedtags ) {
 		global $allowedtags;
 
-		$allowedtags['em'] = array();
+		$allowedtags['em']     = array();
 		$allowedtags['strong'] = array();
-		$allowedtags['ol'] = array();
-		$allowedtags['li'] = array();
-		$allowedtags['ul'] = array();
+		$allowedtags['ol']     = array();
+		$allowedtags['del']    = array();
+		$allowedtags['li']     = array();
+		$allowedtags['ul']     = array();
 		$allowedtags['blockquote'] = array();
-		$allowedtags['code'] = array();
-		$allowedtags['pre'] = array();
-		$allowedtags['a'] = array(
-			'href' => array(),
-			'title' => array(),
+		$allowedtags['code']   = array();
+		$allowedtags['pre']    = array();
+		$allowedtags['a']      = array(
+			'href'   => array(),
+			'title'  => array(),
 			'target' => array(),
 		);
-		$allowedtags['img'] = array(
+		$allowedtags['img']     = array(
 			'src' => array(),
 		);
-		$allowedtags['b'] = array();
-		$allowedtags['span'] = array(
+		$allowedtags['b']       = array();
+		$allowedtags['span']    = array(
 			'style' => array(),
 		);
-		$allowedtags['p'] = array(
+		$allowedtags['p']       = array(
 			'style' => array()
 		);
-		$allowedtags['br'] = array();
+		$allowedtags['br']      = array();
+		$allowedtags['pre']     = array();
+		$allowedtags['h1']      = array();
+		$allowedtags['h2']      = array();
+		$allowedtags['h3']      = array();
+		$allowedtags['h4']      = array();
+		$allowedtags['address'] = array();
 
 		return apply_filters( 'bp_tinymce_allowedtags', $allowedtags );
+	}
+
+	/**
+	 * Copy the content of the 'message_content' key into 'content'
+	 */
+	function messages_send_hack() {
+		if ( bp_is_messages_component() && isset( $_POST['send'] ) && empty( $_POST['content'] ) && !empty( $_POST['message_content'] ) ) {
+			$_POST['content'] = $_POST['message_content'];
+		}
 	}
 }
 
